cde75bf414183db747ca2b74133097a1476f5b84
Danilo Piñeyro <dpineyro@altimetrik.com>
2018-04-17 17:36:47 -0300

    [Part-2] Refresh delete Test Case Item and Test of Map (BETA)

--- src/aura/ongoingProjectsList/ongoingProjectsListController.js
+++ src/aura/ongoingProjectsList/ongoingProjectsListController.js
@@ -17,12 +17,13 @@
         $A.enqueueAction(action);
     },
     loadDetails: function(component,event,helper){
+        console.log(":S");
         var projectDetails = event.getParam("paramDet");
         var action = component.get("c.getProjectOngoingDetails");
         action.setParams({
             "myId": projectDetails.Id
         });
-        console.log('##', JSON.parse(JSON.stringify(projectDetails)));
+        console.log('#?#', JSON.parse(JSON.stringify(projectDetails)));
         
         action.setCallback(this, function(response) {
             var state = response.getState();
@@ -37,6 +38,7 @@
         $A.enqueueAction(action);
     },
     deleteItem: function(component,event,helper){
+        var listOfTc = component.get("v.selectedProject.Test_Cases__r");
         console.log("He shot me!");
         var deleteItem = event.getParam("parDelete");
         var action = component.get("c.deleteTest");
@@ -44,7 +46,18 @@
         action.setParams({
             "myId": deleteItem.Id
         });
-        console.log("Here I am")
+        console.log("Here I am");
+        var myListOfTc = new Array();
+        for(var i = 0;i<listOfTc.length;i++){
+            if(listOfTc[i].Id != deleteItem.Id){
+                myListOfTc.push(listOfTc[i]);
+            }
+            else{
+                console.log("Este fue el eliminado " + deleteItem.Id );
+            }
+        }
+        console.log('## ',JSON.parse(JSON.stringify(myListOfTc)));
+        component.set("v.selectedProject.Test_Cases__r", myListOfTc);
         $A.enqueueAction(action);
     }
 
