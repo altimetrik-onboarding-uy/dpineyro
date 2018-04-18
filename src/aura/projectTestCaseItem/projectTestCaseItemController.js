8b89c1220eeee08be57d2e6ce514fd94c45ff596
Danilo Piñeyro <dpineyro@altimetrik.com>
2018-04-18 11:25:35 -0300

    [PART-2] Layout responsive Ok

--- src/aura/projectTestCaseItem/projectTestCaseItemController.js
+++ src/aura/projectTestCaseItem/projectTestCaseItemController.js
@@ -8,5 +8,15 @@
 		console.log('##', JSON.parse(JSON.stringify(ammunition)));
 		console.log("Fire on the hold");
 		toFire.fire();
+	},
+	formUpSert: function(component, event, helper){
+		var arrow = component.get("v.testCaseItem");
+		var send = component.getEvent('ItemUpSert');
+		console.log('##',JSON.parse(JSON.stringify(arrow)));
+		send.setParams({
+			"parUpsert": arrow
+		});
+		console.log('fire on the hold');
+		send.fire();
 	}
 })
