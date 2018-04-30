84ccc55d4c43199bdb09cef37a2a44a549860c99
Danilo Piñeyro <dpineyro@altimetrik.com>
2018-04-30 14:28:10 -0300

    [Part-3] move to helper ondrop ondrag

--- src/aura/ListItemDaD/ListItemDaDController.js
+++ src/aura/ListItemDaD/ListItemDaDController.js
@@ -1,71 +1,9 @@
 ({
 	onDragStart : function(component, event, helper) {
-        event.dataTransfer.dropEffect = "move";
-        var item = component.get('v.item');
-        event.dataTransfer.setData('text', JSON.stringify(item.Id));
-        var throwarrow = component.getEvent("changeStepIndex");
-        var arrow = component.get("v.item");
-        throwarrow.setParams({
-            "parIndex": arrow
-        });
-        throwarrow.fire();
+        helper.onDragStart(component);
 	},
     onDrop	:	function(component,event,helper){
-        event.preventDefault();
-        var stepList = component.get('v.stepList');
-        var itemDrag = component.get("v.itemDrag");
-        var itemDrop = component.get("v.item");
-        var stepListRes =[];
-        var itemDragIndex = 0;
-        var itemDropIndex = 0;
-        var orderNumber = 0;
-        itemDropIndex = helper.findIdx(itemDrop,stepList);
-        itemDragIndex = helper.findIdx(itemDrag,stepList);
-        stepListRes = helper.reOrder(itemDropIndex,itemDragIndex,itemDrop,itemDrag,stepList);
-        var throwList = component.getEvent("changeStepList");
-        throwList.setParams({
-            "parList": stepListRes
-        });
-        throwList.fire();
-        if((itemDropIndex + 1) >= stepList.length){
-            console.log('Value of Drop'+(stepList[itemDropIndex].Order__c));
-            orderNumber = (stepList[itemDropIndex-1].Order__c+1);
-            console.log('Value of Order Number '+orderNumber);
-        }
-        else{
-            if((itemDropIndex-1) < 0){
-                orderNumber = (stepList[1].Order__c - 1);
-            }else{
-                if(itemDragIndex > itemDropIndex){
-                    orderNumber = ((stepList[itemDropIndex-1].Order__c+stepList[itemDropIndex].Order__c)/2);
-                }
-                if(itemDragIndex < itemDropIndex){
-                        orderNumber = ((stepList[itemDropIndex+1].Order__c+stepList[itemDropIndex].Order__c)/2);
-                }
-            }
-        }
-        console.log(orderNumber);
-        var action = component.get("c.updateOrder");
-        itemDrag.Order__c = orderNumber;
-        action.setParams({
-            "myTCS" : itemDrag
-        });
-        action.setCallback(this, function(response) {
-            var state = response.getState();
-            if (state === "SUCCESS") {
-                component.set("v.stepList", response.getReturnValue());
-            }
-            else {
-                console.log("Failed with state: " + state);
-            }
-        });
-        $A.enqueueAction(action);
-        var throwarrow = component.getEvent("updateStepList");
-        var arrow = component.get("v.stepList");
-        throwarrow.setParams({
-            "parListUpdate": arrow
-        });
-        throwarrow.fire();
+       helper.onDrop(component,event);
     },
     allowDrop : function(component,event,helper){
         event.preventDefault();
